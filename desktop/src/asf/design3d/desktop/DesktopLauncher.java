9132dc242ad434ecc4717e7d9a335378872b17eb
Daniel Strong <ASneakyFox@users.noreply.github.com>
2017-01-14 16:04:16 -0500

    Initial commit

---
+++ desktop/src/asf/design3d/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package asf.design3d.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import asf.design3d.Design3dApp;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new Design3dApp(), config);
+	}
+}
